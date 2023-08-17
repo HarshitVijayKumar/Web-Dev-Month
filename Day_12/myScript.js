d8f7aa659971dd48685778c378d3401c0fafe685
HarshitVijayKumar <91574216+HarshitVijayKumar@users.noreply.github.com>
2023-08-17 21:41:57 +0530

    Added Day_12 material

---
+++ Day_12/myScript.js
@@ -0,0 +1,45 @@
+function reveal_D1(){
+    document.getElementById('D1').innerHTML= "This is a month dedicated to <ins>Web Dev</ins>. Every day I have to learn something new and add to this site.";
+}
+function reveal_D2(){
+    document.getElementById("D2").innerHTML = "What follows are paragraphs about the things I learnt on a day and my thoughts on that day. What will be attached with everyday is a screenshot of how the site looked each day.";
+}
+function reveal_D2_2(){
+    document.getElementById("D2(2)").innerHTML="Day-1 covered the basics of HTML, I learnt the layout of a webpage, learnt how to add paragraphs, images, links and how to control were those links open their webpages at.<br> I also learnt how to add borders although they were not working very well yet. I discovered titles. Anything you write there pops up when you hover over it, I have planned to use it to keep track of the elements and the day they were added";
+}
+function reveal_D3(){
+    document.getElementById("D3").innerHTML="Day-2 began with CSS and using a CSS file to change the whole style of the webpage. I settled on External CSS.<br> Then I learned how to make images into links and transformed my insta and blog link to an image.<br> I also attempted to make a button into a link but that part seemed to be trickier than it looked.<br> Than I created a bookmark and decided on the actual content of the site. The site will document the chages done <br> everyday and will have a photo of those changes attached alongside them.";
+}
+function reveal_D4(){
+    document.getElementById('D4').innerHTML='On Day-3, I learned image maps, I edited the cat picture to include a Sharengan. Click the eye for a surprise!<br> I also made the icon for the webpage which is a cat peeking at you!';
+}
+function reveal_D5(){
+    document.getElementById('D5').innerHTML='Day-4 was hard, it was hard to focus and complete the day. I was only able to add a table which had problems in adding the image in it and setting its size. The Web-Dev Month has also started consuming a lot of time and I am wondering to restrict the time I invest each day. <br> I was also able to design a navigation bar<br>';
+}
+function reveal_D6(){
+    document.getElementById('D6').innerHTML='I finally fixed the table, the images finally fit into the columns better. I discovered Iframes and implemented them now using that to display the table fully.<br> I also added a list at the end of the webpage.<br> I also used JavaScript to add a credits button in the end.<br>';
+}
+function reveal_D7(){
+    document.getElementById('D7').innerHTML='Learned about the meta tags and added them.<br> Modified the Layout,<br> Retired Early.<br>';
+}
+function reveal_D8(){
+    document.getElementById('D8').innerHTML='The site is now responsive.<br> The picture element on the cat now works, displays a smaller cat on shrinking the viewport.<br> Media Queries were added which changed the whole design layout further.<br>';
+}
+function reveal_D9(){
+    document.getElementById('D9').innerHTML='Rest Day<br>';
+}
+function reveal_D10(){
+    document.getElementById('D10').innerHTML='Finished Learning about URLs and Encoding.<br>';
+}
+function reveal_D11(){
+    document.getElementById('D11').innerHTML='Added a form elements to create the website but was not abe to connect to a page that can process the response<br> Learned about various form attributes and elements.<br>';
+}
+function reveal_D12(){
+    document.getElementById('D12').innerHTML='Modified the Layout.<br> Finished learning about forms.<br>';
+}
+function threaten(){
+    alert("Meow Meow I know where you sleep!");
+}
+function credits(){
+    alert("Made by Harshit Vijay Kumar. Buy him a cup o' tea eh!");
+}
